60e8dcd578c803590a025f46630b05a66d743a84
Giebisch <rafael@giebisch-mail.de>
2020-02-05 22:43:24 +0100

    Created Scraper, added README, example and requirements

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ATPGameScraper
 Python scraper for getting all games played by a specific ATP player or the top 50
+Data is getting scraped from the Official Site of Men's Professional Tennis www.atptour.com
+
+# Installation
+Make sure to have python3 installed. Download files and run 
+
+    pip install -r requirements.txt
+
+or on Ubuntu
+
+    pip3 install -r requirements.txt
+
+# Usage
+To download all the games of the top 50 players simply run the scraper with
+
+    python scraper.py
+
+or on Ubuntu with
+
+    python3 scraper.py
+
+To get only the games froma specific player, give an additional argument with link of the player, example:
+
+    python scraper.py https://www.atptour.com/en/players/novak-djokovic/d643/overview
+
+Get the link from the official atptour.com website.
+
+# Result
+The scraper will automatically create a *.csv* file with all the games listed. Example:
+
+    Novak Djokovic;Tokyo;Finals;John Millman;80;W;63 62
+    Novak Djokovic;Tokyo;Semi-Finals;David Goffin;15;W;63 64
+    Novak Djokovic;Tokyo;Quarter-Finals;Lucas Pouille;24;W;61 62
+    Novak Djokovic;Tokyo;Round of 16;Go Soeda;133;W;63 75
+    Novak Djokovic;Tokyo;Round of 32;Alexei Popyrin;94;W;64 62
+    Novak Djokovic;US Open;Round of 16;Stan Wawrinka;24;L;46 57 12 (RET)
+    Novak Djokovic;US Open;Round of 32;Denis Kudla;111;W;63 64 62
+    Novak Djokovic;US Open;Round of 64;Juan Ignacio Londero;56;W;64 763 61
+    Novak Djokovic;US Open;Round of 128;Roberto Carballes Baena;76;W;64 61 64
+    ...
---
+++ scraper.py
@@ -0,0 +1,84 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import datetime
+
+class Game:
+    """
+    Contains all the important stats about one game
+    Can give back a formatted line for csv files
+    """
+    def __init__(self, player, tournament, round, opponent, rank, w_l, score):
+        self.player = player
+        self.tournament = tournament
+        self.round = round
+        self.opponent = opponent
+        self.rank = rank
+        self.w_l = w_l
+        self.score = score
+    def get_data(self):
+        data = ";".join([self.player, self.tournament, self.round, \
+            self.opponent, self.rank, self.w_l, self.score])
+        return data
+
+def scrape_person(url, output):
+    """
+    Takes an url for a specific player as an input and scrapes all the games
+    from the atp website
+    If output == true, games will be written to csv file
+    """
+    games_played = []
+    # get link to all games played
+    url = url[:url.find("/overview")] + "/player-activity?year=all"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    player_name = soup.find(class_="first-name").text + " " + soup.find(class_="last-name").text
+    tournaments = soup.find_all("div", class_="activity-tournament-table")
+    for tournament in tournaments:
+        tournament_name = tournament.find(class_="tourney-title").text.strip().strip()
+        content = tournament.find("table", class_="mega-table").find("tbody")
+        for game in content.find_all("tr"):
+            game_stats = game.find_all("td")
+            round, rank, opponent, w_l, score = game_stats
+            opponent = opponent.find(class_="mega-player-name")
+            games_played.append(Game(player_name, tournament_name, round.text, \
+                opponent.text, rank.text.strip(), w_l.text.strip(), score.text.strip()))
+    if output:
+        with open("atp_" + player_name.replace(" ", "_") + "_" + \
+            datetime.datetime.now().strftime("%d_%m_%Y") + ".csv", "w") as out:
+            for game in games_played:
+                out.write(game.get_data() + "\n")
+    else:
+        return games_played
+
+def scrape_top_50():
+    """
+    Searches on the atptour website for the top 50 players and 
+    calls scrape_person for each one
+    Write all collected games to csv file after
+    """
+    all_players = []
+    url = "https://www.atptour.com/en/rankings/singles"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    content = soup.find("table", class_="mega-table").find("tbody")
+    for player in content.find_all("tr"):
+        href = player.find(class_="player-cell").find("a")["href"]
+        player_url = "https://www.atptour.com" + href
+        all_players.append(scrape_person(player_url, False))
+        print("Getting games for: " + all_players[-1][0].player)
+    with open("atp_top_50_" + datetime.datetime.now().strftime("%d_%m_%Y") + ".csv", "w") as out:
+        for player in all_players:
+            for game in player:
+                out.write(game.get_data() + "\n")
+        
+
+if __name__ == "__main__":
+    # example input "https://www.atptour.com/en/players/novak-djokovic/d643/overview"
+    if len(sys.argv) == 1:
+        scrape_top_50()
+    elif len(sys.argv) == 2:
+        # True, if only one person has to be scraped
+        scrape_person(sys.argv[1], True)
+    else:
+        print("Error with argument.")
